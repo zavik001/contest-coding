19000babc561b10d1bb04ee10ef0305a4205313e
Zavik <zav021104@gmail.com>
2024-12-31 13:17:26 +0700

    feat(leetcode): add solution for problem 516

---
+++ leetcode/516/Solution.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/longest-palindromic-subsequence/
+
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i+1][j]);
+                }
+            }
+        }
+
+        return dp[0][n-1];
+    }
+}
