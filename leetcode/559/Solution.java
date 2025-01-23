53480c453d85e939acc537046a0fb9d8d025cec2
Zavik <zav021104@gmail.com>
2025-01-23 12:00:00 +0700

    feat(leetcode): add solution for problem 559

---
+++ leetcode/559/Solution.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/maximum-depth-of-n-ary-tree/description/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int maxDepth = 0;
+        for (Node child : root.children) {
+            int childDepth = maxDepth(child);
+            if (childDepth > maxDepth) {
+                maxDepth = childDepth;
+            }
+        }
+        
+        return maxDepth + 1;
+    }
+}
