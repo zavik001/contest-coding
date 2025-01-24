ec121ab9075938b2623c5e0cca0391e4aaa5fe11
Zavik <zav021104@gmail.com>
2025-01-24 11:42:50 +0700

    feat(leetcode): add solution for problem 435

---
+++ leetcode/435/Solution.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/non-overlapping-intervals/description/
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) return 0;
+        
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[1]));
+        
+        int count = 0;
+        int end = intervals[0][1];
+        
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < end) {
+                count++;
+            } else {
+                end = intervals[i][1];
+            }
+        }
+        
+        return count;
+    }
+}
