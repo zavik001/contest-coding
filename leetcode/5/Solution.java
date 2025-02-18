a8efaad8f5d7e7cc66ba362f8882afb39c7132be
Zavik <zav021104@gmail.com>
2025-02-18 12:33:48 +0700

    feat: add solution for problem 5

---
+++ leetcode/5/Solution.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/longest-palindromic-substring/
+
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        
+        int start = 0, end = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = polindrome(s, i, i);
+            int len2 = polindrome(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        
+        return s.substring(start, end + 1);
+    }
+    
+    private int polindrome(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        return r - l - 1;
+    }
+}
